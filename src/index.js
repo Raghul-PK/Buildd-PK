8e3dd21591ddf2716393038f7dc1d87adcf95493
Raghul PK <pkraghul2001@gmail.com>
2024-05-09 11:02:13 +0530

    Routing added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
-  </React.StrictMode>
+  <HashRouter>
+    <App/>
+  </HashRouter>
 );
